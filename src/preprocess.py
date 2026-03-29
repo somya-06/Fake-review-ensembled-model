38aeb82a1a98bbd65a17d03e282b0e7cc1be436c
somya-06 <somyagoel062005@gmail.com>
2026-03-29 21:38:53 +0530

    Updated file

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,23 +1,4 @@
 import re
-<<<<<<< HEAD
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-
-nltk.download('stopwords')
-nltk.download('wordnet')
-
-def clean_text(text):
-    text = text.lower()
-    text = re.sub(r'[^a-zA-Z]', ' ', text)
-    words = text.split()
-    
-    stop_words = set(stopwords.words('english'))
-    lemmatizer = WordNetLemmatizer()
-    
-    words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
-    return ' '.join(words)
-=======
 
 def clean_text(text):
     # 1. Handle non-string inputs
@@ -35,4 +16,3 @@ def clean_text(text):
     text = " ".join(text.split())
     
     return text
->>>>>>> teammate/main
